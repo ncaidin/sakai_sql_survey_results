b94d125d2357f1d4140b510280a1c03973577952
ncaidin <neal.caidin@gmail.com>
2014-05-18 10:44:52 -0400

    Rewrote process_events_files.py so that it is not necessary to manually calculate the relative percentages of the events in the CSV file. Instead this Python script does that for you. Will save a lot of manual work.

--- process_events_files.py
+++ process_events_files.py
@@ -4,12 +4,11 @@ import csv
 
 path = '/Users/nealcaidin/Documents/tmp/Events'
 
-# create an events dictionary
-events = {}
-events_summary = {}
+# create events dictionaries. One for the total across all files for the detail events,
+#    and another to rollup the events to their "parent" functionality.
+aggregate_detail = {}
+rollup_aggregate_detail = {}
 
-# keep track of the total events so we can calculate percentages at the end
-total_proportions = 0
 
 for file in os.listdir(path):
     current = os.path.join(path, file)
@@ -18,6 +17,14 @@ for file in os.listdir(path):
         reader = csv.reader(csv_file)
         # reset the total events. We will use this to calculate percentages
         i = 0
+
+        # initialize total number of events that are counted for the current file.
+        total_events_in_file = 0
+
+        # reset dictionary for this file
+        events_per_file = {}
+        rollup_events_per_file = {}
+
         # catching exceptions helps to clean up stray
         # characters in the data
         try:
@@ -25,36 +32,28 @@ for file in os.listdir(path):
 
                 i += 1
                 # skip the first row, header
+
+
                 if i > 1 :
-                    # the third column is index [2]
-                    # it contains proportion (percent) of event hits
-                    #    compared to other events in the file
-                    # print("row[1] " + row[1])
-                    # print ("row[1] " + row[1])
-                    proportion = float(row[2].replace(",",""))
-                    total_proportions += proportion
+
+                    # get the raw number in row[0]. The first column of this file has the number of "hits" of
+                    # event.
 
                     # clean up event name. Strip whitespace.
-                    event_name = row[1].strip()
+                    detail_event_name = row[1].strip()
+
+                    # proportion = float(row[2].replace(",",""))
+                    events_per_file[detail_event_name] = float(row[0])
+                    total_events_in_file += events_per_file[detail_event_name]
 
                     # events have names like content.read and content.revise
                     #   by taking the prefix (the part before the period)
-                    #   we can summarize all the events of the same type
-                    event_name_summary = event_name.split(".")[0]
-
-                    # adding up the proportions across all files
-                    # for a given event
-                    if events.has_key(event_name):
-                        events[event_name] += proportion
+                    #   we can summarize all the events of the same type (i.e. "content")
+                    rollup_event_name = detail_event_name.split(".")[0]
+                    if rollup_events_per_file.has_key(rollup_event_name):
+                        rollup_events_per_file[rollup_event_name] += events_per_file[detail_event_name]
                     else:
-                        events[event_name] = proportion
-
-                    # adding up the proportions across all files
-                    # for a given event summary (rollup)
-                    if events_summary.has_key(event_name_summary):
-                        events_summary[event_name_summary] += proportion
-                    else:
-                        events_summary[event_name_summary] = proportion
+                        rollup_events_per_file[rollup_event_name] = events_per_file[detail_event_name]
 
 
         except csv.Error:
@@ -66,13 +65,40 @@ for file in os.listdir(path):
             print('csv choked on line %s' % (i+1))
             raise
 
-for event in events:
-    print event, "," , events[event]
+        # Now we go through all the events read from that file
+        # calculate the relative proportion to the total events in that file
+        # to normalize the data, and add that number to an aggregate total across
+        # all files.
+        for event_name in events_per_file:
+            # calculate the proportion (percentage) of the event relative to total of all events in file
+            proportion = events_per_file[event_name] / total_events_in_file
 
-print "total", ",",  str(total_proportions)
+            # add into an aggregate count across all files
+            if aggregate_detail.has_key(event_name):
+                # calculating the percentage of events relative to all events in the file
+                aggregate_detail[event_name] += proportion
+            else:
+                # first time to see this event across files read so far.
+                aggregate_detail[event_name] = proportion
 
-for event_summary in events_summary:
-    print event_summary, ",", events_summary[event_summary]
+
+            # now for the rollup aggregates. i.e. content.read and content.write events are under "content"
+            # first get the aggregate name, which is the first (zero element) of the full name separated by periods.
+            rollup_event_name = event_name.split(".")[0]
+
+            if rollup_aggregate_detail.has_key(rollup_event_name):
+                rollup_aggregate_detail[rollup_event_name] += proportion
+            else:
+                rollup_aggregate_detail[rollup_event_name] = proportion
+
+
+for event_name in aggregate_detail:
+    print event_name, "," , aggregate_detail[event_name]
+
+print "------------------------------------"
+
+for event_rollup in rollup_aggregate_detail:
+    print event_rollup, ",", rollup_aggregate_detail[event_rollup]
 
 
 
