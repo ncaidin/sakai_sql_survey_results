a2dec20cd78efcbbf77e54c91e2c31ba305f8a8c
ncaidin <neal.caidin@gmail.com>
2014-05-18 22:08:30 -0400

    Updated the file to calculate the relative percentages of each tool, both for sites and for participants. Two changes compared to last version of the file. In the previous version you had to manually update the CSV file with the relative percentages. Also, the first script only used the participants access to the tools, not the sites (that's because it was manually calculated). No more. This version outputs two columns: aggregate for tools deployed by site, and by participants.

--- process_tools_files.py
+++ process_tools_files.py
@@ -4,12 +4,10 @@ import csv
 
 path = '/Users/nealcaidin/Documents/tmp/Tools'
 
-# create an events dictionary
-events = {}
-events_summary = {}
+# create a dictionary to hold the sum of all percentages of tool use across file
+#  (both by sites and by participants using a Python tuple construct)
+aggregate_tools_percentages = {}
 
-# keep track of the total events so we can calculate percentages at the end
-sum_proportions = 0
 
 for file in os.listdir(path):
     current = os.path.join(path, file)
@@ -18,6 +16,15 @@ for file in os.listdir(path):
         reader = csv.reader(csv_file)
         # reset the total events. We will use this to calculate percentages
         i = 0
+
+        # initialize total number of tool placements that are counted for the current file, both based on
+        # number of sites and based on the number of participants in the sites.
+        total_by_sites_in_file = 0
+        total_by_participants_in_file = 0
+
+        #reset dictionary which will contain the data for each tool
+        tools_per_file = {}
+
         # catching exceptions helps identify data issues
         # which need cleaning up
         try:
@@ -26,21 +33,20 @@ for file in os.listdir(path):
                 i += 1
                 # skip first row - header
                 if i > 1 :
-                    # some string literals representing numbers
-                    # had commas in them. Needed to remove commas
-                    # convert to number, which is a proportion
-                    proportion = float(row[3].replace(",",""))
-                    sum_proportions += proportion
 
-                    # strip out extra whitespace
-                    event_name = row[0].strip()
+                    # clean up any excess white space in the tool_name
+                    tool_name = row[0].strip()
 
+                    tool_num_sites = float(row[1])
+                    tool_num_participants = float(row[2])
 
-                    # add up the proportions for all the events across files
-                    if events.has_key(event_name):
-                        events[event_name] += proportion
-                    else:
-                        events[event_name] = proportion
+                    # add to totals so we can figure out proportion (percentages) later
+                    total_by_sites_in_file += tool_num_sites
+                    total_by_participants_in_file += tool_num_participants
+
+                    # save data for each tool from the file
+                    tools_per_file[tool_name] = (tool_num_sites, tool_num_participants)
+
 
         except csv.Error:
             print current
@@ -55,10 +61,24 @@ for file in os.listdir(path):
             print('csv choked on line %s' % (i+1))
             raise
 
-for event in events:
-    print event, "," , events[event]
+        # now we can calculate the relative proportion (percentage) of tool use both
+        # with respect to sites and with respect to number of participants, for each tool
+        for one_tool in tools_per_file:
+            # calculate the proportions (percentages)
+            tools_site_percentage = tools_per_file[one_tool][0] / total_by_sites_in_file
+            tools_participants_percentage = tools_per_file[one_tool][1] / total_by_participants_in_file
 
-print "total", ",",  str(sum_proportions)
+            if aggregate_tools_percentages.has_key(one_tool):
+                aggregate_tools_percentages[one_tool][0] += tools_site_percentage
+                aggregate_tools_percentages[one_tool][1] += tools_participants_percentage
+            else:
+                aggregate_tools_percentages[one_tool] = [tools_site_percentage, tools_participants_percentage]
+            
+
+for a_tool in aggregate_tools_percentages:
+    # zero index is by sites, and one index is by participants
+    print a_tool, "," , aggregate_tools_percentages[a_tool][0], ",", aggregate_tools_percentages[a_tool][1]
+
 
 
 
